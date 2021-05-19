ba84f6d9547baec41bd390fef9ab3cf23c109fbd
SamvitaKarkal <samvitaskarkal@gmail.com>
2021-05-19 12:34:21 +0530

    Added notification element

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,19 +1,31 @@
-import logger from "js-logger";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import { initializeLogger } from "./common/logger";
 
+import Dashboard from "components/Dashboard";
+import PageLoader from "components/PageLoader";
+
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     initializeLogger();
-    logger.info("Log from js-logger");
   }, []);
+
+  if (loading) {
+    return (
+      <div className="h-screen">
+        <PageLoader />
+      </div>
+    );
+  }
 
   return (
     <Router>
+      <ToastContainer />
       <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
-        <Route exact path="/about" render={() => <div>About</div>} />
+        <Route exact path="/" component={Dashboard} />
       </Switch>
     </Router>
   );
